90f56a33456e6e21e6a2e283e570c52c3e5648a7
Bao ta <bao.ta@s3corp.com.vn>
2025-01-20 16:39:30 +0700

    Feature(Wishlist): Move product from wishlist to cart

--- app/Http/Controllers/WishlistController.php
+++ app/Http/Controllers/WishlistController.php
@@ -36,4 +36,15 @@ class WishlistController extends Controller
 
         return redirect()->back();
     }
+
+    public function move_to_cart($rowId)
+    {
+        $item = Cart::instance('wishlist')->get($rowId);
+
+        Cart::instance('wishlist')->remove($rowId);
+        Cart::instance('cart')->add($item->id, $item->name, $item->qty, $item->price)
+            ->associate('App\Models\Product');
+
+        return redirect()->back();
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -25,9 +25,10 @@ Route::delete('/cart/remove/{rowId}', [CartController::class, 'remove_item'])->n
 Route::delete('/cart/clear', [CartController::class, 'empty_item'])->name('cart.empty');
 
 Route::get('/wishlist', [WishlistController::class, 'index'])->name('wishlist.index');
-Route::post('/wishlist/add', [WishListController::class, 'add_to_wishlist'])->name('wishlist.add');
-Route::delete('/wishlist/item/remove/{rowId}', [WishListController::class, 'remove_item'])->name('wishlist.item.remove');
-Route::delete('/wishlist/clear', [WishListController::class, 'empty_wishlist'])->name('wishlist.item.clear');
+Route::post('/wishlist/add', [WishlistController::class, 'add_to_wishlist'])->name('wishlist.add');
+Route::delete('/wishlist/item/remove/{rowId}', [WishlistController::class, 'remove_item'])->name('wishlist.item.remove');
+Route::delete('/wishlist/clear', [WishlistController::class, 'empty_wishlist'])->name('wishlist.item.clear');
+Route::post('/wishlist/move-to-cart/{rowId}', [WishlistController::class, 'move_to_cart'])->name('wishlist.move.to.cart');
 
 Route::middleware('auth')->group(function () {
     Route::get('/account-dashboard', [UserController::class, 'index'])->name('user.index');
